acc38b33731e85707c9cb27e36210eebdf703ebd
i-yuki23 <yuki.ito.s7@dc.tohoku.ac.jp>
2024-04-04 09:42:18 +0900

    refactored classify_water.py to properly catch the exception

--- src/preprocess/classify_water.py
+++ src/preprocess/classify_water.py
@@ -26,63 +26,62 @@ import traceback
 from lib.voxel import get_voxel_info
 from lib.pdb import get_coordinates_from_pdb, get_all_pdb_names
 from lib.path import get_water_path
+from lib.helper import make_dir
 from WaterClassifier.WaterClassifier import WaterClassifier
 from WaterClassifier.ClassifyingRuleFactory import ClassifyingRuleFactory
 from WaterClassifier.LigandPocketDefinerFactory import LigandPocketDefinerFactory
 DATA_DIR = "/home/ito/research/data/labeled_water/"
 
-def classify_water(ligand_voxel_num, classifying_rule_name, ligand_pocket_definer_name):
+def classify_water(pdb_name, ligand_voxel_num, classifying_rule_name, ligand_pocket_definer_name):
     
     PATH_TYPE = f"{classifying_rule_name}/{ligand_pocket_definer_name}/ligand_pocket_voxel_num_{ligand_voxel_num}/"
-    pdb_names = get_all_pdb_names()
-    for pdb_name in pdb_names:
         # pdb_name = '4b74'
-        print(pdb_name)
+    print(pdb_name)
 
-        paths = {
-                "output_displaceable": os.path.join(DATA_DIR, PATH_TYPE, "displaceable/", pdb_name,  f"pred_O_placed_{pdb_name}_3.0.pdb"),
-                "output_non_displaceable": os.path.join(DATA_DIR, PATH_TYPE, "non_displaceable/", pdb_name,  f"pred_O_placed_{pdb_name}_3.0.pdb"),
-            }
-        for path in paths.values():
-            # os.path.dirname(path) を使ってファイルのあるディレクトリのパスを取得
-            directory = os.path.dirname(path)
-            
-            # ディレクトリが存在するか確認
-            if not os.path.exists(directory):
-                # ディレクトリが存在しない場合、ディレクトリを作成（親ディレクトリも含めて）
-                os.makedirs(directory, exist_ok=True)  #
+    paths = {
+            "output_displaceable": os.path.join(DATA_DIR, PATH_TYPE, "displaceable/", pdb_name,  f"pred_O_placed_{pdb_name}_3.0.pdb"),
+            "output_non_displaceable": os.path.join(DATA_DIR, PATH_TYPE, "non_displaceable/", pdb_name,  f"pred_O_placed_{pdb_name}_3.0.pdb"),
+        }
+    
+    for path in paths.values():
+        make_dir(path)
 
-        water_path = get_water_path(pdb_name)
-        water_coordinates = get_coordinates_from_pdb(water_path)
-        grid_dims, grid_origin = get_voxel_info(pdb_name)
+    water_path = get_water_path(pdb_name)
+    water_coordinates = get_coordinates_from_pdb(water_path)
+    grid_dims, grid_origin = get_voxel_info(pdb_name)
 
-        try:
-            water_classifier = WaterClassifier(pdb_name, grid_dims, grid_origin)
-            
-            classifying_rule_factory = ClassifyingRuleFactory()
-            ligand_pocket_definer_factory = LigandPocketDefinerFactory()
-            ligand_pocket_definer = ligand_pocket_definer_factory.get_ligand_pocket_definer(ligand_pocket_definer_name, pdb_name, grid_dims, grid_origin, ligand_voxel_num)
-            water_classifying_rule = classifying_rule_factory.get_rule(classifying_rule_name, pdb_name, grid_dims, grid_origin)
-            water_classifier.define_ligand_pocket(ligand_pocket_definer)
-            water_classifier.create_convert_dict(water_coordinates)
-            displaceable_water_ids, non_displaceable_water_ids = water_classifier.get_classified_water_ids(water_coordinates, water_classifying_rule)
-            water_classifier.save_classified_water_as_pdb(displaceable_water_ids=displaceable_water_ids,
-                                                    non_displaceable_water_ids=non_displaceable_water_ids,
-                                                    output_path_displaceable=paths["output_displaceable"],
-                                                    output_path_non_displaceable=paths["output_non_displaceable"])
-        except Exception as e:
-            print(f"Error: {e}\n{traceback.format_exc()}")
-            continue
-        # exit()
-        # break
+    water_classifier = WaterClassifier(pdb_name, grid_dims, grid_origin)
+    
+    classifying_rule_factory = ClassifyingRuleFactory()
+    ligand_pocket_definer_factory = LigandPocketDefinerFactory()
+    ligand_pocket_definer = ligand_pocket_definer_factory.get_ligand_pocket_definer(ligand_pocket_definer_name, pdb_name, grid_dims, grid_origin, ligand_voxel_num)
+    water_classifying_rule = classifying_rule_factory.get_rule(classifying_rule_name, pdb_name, grid_dims, grid_origin)
+    water_classifier.define_ligand_pocket(ligand_pocket_definer)
+    water_classifier.create_convert_dict(water_coordinates)
+    displaceable_water_ids, non_displaceable_water_ids = water_classifier.get_classified_water_ids(water_coordinates, water_classifying_rule)
+    water_classifier.save_classified_water_as_pdb(displaceable_water_ids=displaceable_water_ids,
+                                            non_displaceable_water_ids=non_displaceable_water_ids,
+                                            output_path_displaceable=paths["output_displaceable"],
+                                            output_path_non_displaceable=paths["output_non_displaceable"])
+
 
 if __name__ == '__main__':
     ligand_voxel_nums = [10, 9, 8, 6, 4]
     classifying_rule_names = ["WaterClassifyingRuleCenter", "WaterClassifyingRuleSurface"]
     ligand_pocket_definer_names = ["LigandPocketDefinerGhecom", "LigandPocketDefinerOriginal"]
+    pdb_names = get_all_pdb_names()
 
     for ligand_voxel_num in ligand_voxel_nums:
         for classifying_rule_name in classifying_rule_names:
             for ligand_pocket_definer_name in ligand_pocket_definer_names:
-                print(f"ligand_voxel_num: {ligand_voxel_num}, classifying_rule: {classifying_rule_name}, ligand_pocket_definer: {ligand_pocket_definer_name}")
-                classify_water(ligand_voxel_num, classifying_rule_name, ligand_pocket_definer_name)
+                for pdb_name in pdb_names:
+                    print(f"ligand_voxel_num: {ligand_voxel_num}, classifying_rule: {classifying_rule_name}, ligand_pocket_definer: {ligand_pocket_definer_name}")
+                    try:
+                        classify_water(pdb_name, ligand_voxel_num, classifying_rule_name, ligand_pocket_definer_name)
+                    except ValueError as e:
+                        # 特定のエラーを捕捉して処理。ここではエラーメッセージを出力するだけ
+                        print(f"Error processing {pdb_name}: {e}")
+                        continue
+                    except Exception as e:
+                        # 予期しない他のエラーを捕捉して処理
+                        print(f"Unexpected error processing {pdb_name}: {e}")
