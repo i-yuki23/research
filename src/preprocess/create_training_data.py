7ee785ce22d015755b5711943517cd2ba7a31a73
i-yuki23 <yuki.ito.s7@dc.tohoku.ac.jp>
2024-04-11 09:05:14 +0900

    created create_training_data.py

--- src/preprocess/create_training_data.py
+++ src/preprocess/create_training_data.py
@@ -1,16 +1,9 @@
-"""
-水分子の周囲10ボクセルのgrのボクセルデータを取ってくる
-
-手順
-1. pdbから水分子の座標を取ってくる
-2. 座標をボクセルのインデックスに変換
-3. 周囲10ボクセルのgrを取得
-"""
 import sys
 sys.path.append('..')
-import os
-import numpy as np
+from lib.pdb import get_all_pdb_names
 from Class.TrainingDataCreator.TrainingDataCreatorGr import TrainingDataCreatorGr
+from Class.TrainingDataCreator.TrainingDataCreatorGist import TrainingDataCreatorGist
+
 
 LIGNAD_POCKET_VOXEL_NUM = 9
 CLASSIFYING_RULE = "WaterClassifyingRuleSurface"
@@ -18,8 +11,34 @@ LIGAND_POCKET_DEFINER = "LigandPocketDefinerOriginal"
 DATA_VOXEL_NUM = 10
 
 def main():
-    training_data_creator = TrainingDataCreatorGr(pdb_name="4lkk", ligand_voxel_num=LIGNAD_POCKET_VOXEL_NUM, classifying_rule=CLASSIFYING_RULE, ligand_pocket_definer=LIGAND_POCKET_DEFINER, data_voxel_num=DATA_VOXEL_NUM)
-    training_data_creator.save_training_data()
+    pdb_names = get_all_pdb_names()
+
+    for pdb_name in pdb_names:
+        # pdb_name = '3kr4'
+        print(pdb_name)
+
+        try:
+            training_gr_data_creator = TrainingDataCreatorGr(pdb_name=pdb_name, ligand_voxel_num=LIGNAD_POCKET_VOXEL_NUM, classifying_rule=CLASSIFYING_RULE, ligand_pocket_definer=LIGAND_POCKET_DEFINER, data_voxel_num=DATA_VOXEL_NUM)
+            training_gr_data_creator.save_training_data()
+        except ValueError as e:
+            print(f"Error processing {pdb_name}: {e}")
+        except FileNotFoundError as e:
+            print(f"Error processing {pdb_name}: {e}")
+        except Exception as e:
+            print(f"Error processing {pdb_name}: {e}")
+            exit(1)
+        
+        try:
+            training_gist_data_creator = TrainingDataCreatorGist(pdb_name=pdb_name, ligand_voxel_num=LIGNAD_POCKET_VOXEL_NUM, classifying_rule=CLASSIFYING_RULE, ligand_pocket_definer=LIGAND_POCKET_DEFINER, data_voxel_num=DATA_VOXEL_NUM)
+            training_gist_data_creator.save_training_data()
+        except ValueError as e:
+            print(f"Error processing {pdb_name}: {e}")
+        except FileNotFoundError as e:
+            print(f"Error processing {pdb_name}: {e}")
+        except Exception as e:
+            print(f"Error processing {pdb_name}: {e}")
+            exit(1)
+            
 
 if __name__ == "__main__":
     main()
