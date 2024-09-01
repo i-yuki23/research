165d0ecfcd6710dc31c3f48826b1fc08e653b941
i-yuki23 <yuki.ito.s7@dc.tohoku.ac.jp>
2024-09-01 17:40:19 +0900

    modify count_critical_failure

--- src/analysis/critical_failure/count_critical_failure.py
+++ src/analysis/critical_failure/count_critical_failure.py
@@ -18,13 +18,13 @@ from lib.helper import extract_points_within_threshold
     
 RADIUSES = {'C': 1.69984, 'N': 1.62500, 'O': 1.51369, 'S': 1.78180, 'B' : 1.92, 'F' : 1.47, 'P' : 1.80, 'I' : 1.98}
 MAXIMUM_EMBEDDING_DISTANCE = {'C': 0.7564608700685973, 'O': 1.0939219279500616, 'S': 0.2330085575854186, 'N': 0.9927163533708043, 'B': 0.28465040269571507, 'P': 0.30781603, 'F': 0.6491095237687778, 'I': 0}
-THRESHOLDS = {atom_type: RADIUSES[atom_type] + RADIUSES['O'] - 2 * MAXIMUM_EMBEDDING_DISTANCE[atom_type] for atom_type in RADIUSES.keys()}
+THRESHOLDS = {atom_type: RADIUSES[atom_type] + RADIUSES['O'] - MAXIMUM_EMBEDDING_DISTANCE[atom_type] for atom_type in RADIUSES.keys()}
 # THRESHOLDS = {'C': 1.7006082598628054, 'N': 1.1532572932583913, 'O': 0.8395361440998768, 'S': 2.829472884829163, 'B': 2.8643891946085698, 'F': 1.6854709524624445, 'P': 2.69805794, 'I': 3.49369}
 
 settings = {
         "DATA_TYPE" : 'gr',
         "DATA_VOXEL_NUM" : 10,
-        "CLASSIFYING_RULE" : 'WaterClassifyingRuleEmbedding',
+        "CLASSIFYING_RULE" : 'WaterClassifyingRuleSurface',
         "LIGAND_POCKET_DEFINER" : 'LigandPocketDefinerOriginal',
         "LIGAND_VOXEL_NUM" : 8,
         "MODEL_NAME" : 'ResNet',
@@ -48,15 +48,23 @@ def count_failed_water(pdb_name, model_settings):
         # print(f"pdb_name: {pdb_name}, failed_water_num_total: {failed_water_num_total}")
     return failed_water_num_total
 
-pdb_names = get_pdb_names_by_txt('../../../data/valid_val.txt')
+pdb_names = get_pdb_names_by_txt('../../../data/valid_test.txt')
 failed_protein_num = 0
 total_failed_water_num = 0
+succeed_protein = []
 for pdb_name in pdb_names:
     failed_water_num = count_failed_water(pdb_name, settings)
     if failed_water_num > 0:
         failed_protein_num += 1
+    else:
+        succeed_protein.append(pdb_name)
+
     total_failed_water_num += failed_water_num
 
 
 print(failed_protein_num, '/', len(pdb_names))
 print(total_failed_water_num)
+
+with open(f'../../../data/{settings["CLASSIFYING_RULE"]}_success_protein_test.txt', 'w') as f:
+    for pdb_name in succeed_protein:
+        f.write(pdb_name + '\n')
